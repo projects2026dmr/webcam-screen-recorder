46bf612873bbf5d2be493708dda214d43831de88
projects2026dmr <projects2026.dmr@gmail.com>
2026-07-11 16:06:52 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  base: '/webcam-screen-recorder/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
